bb8b07a6c56c1af56d7c4488c301790ca5278a63
THuongdE25 <nguyenmanhthe2005.thcshaihau@gmail.com>
2026-03-13 14:35:13 +0700

    2.home

---
+++ Frontend/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import CakeList from "./components/CakeList";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <CakeList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
