90145fe6a9272ad4dd5c294a6e0c0f45d1cc5b00
THuongdE25 <nguyenmanhthe2005.thcshaihau@gmail.com>
2026-03-23 22:33:53 +0700

    sua lai catalog

--- Frontend/src/pages/Cakes.jsx
+++ Frontend/src/pages/Cakes.jsx
@@ -2,25 +2,35 @@ import { useEffect, useState } from "react";
 import { getContract } from "../contract/contract";
 import ProductCatalog from "../data/ProductsCatalog";
 import { ethers } from "ethers";
+import { useParams } from "react-router-dom";
 
 function Cakes() {
+  const { category } = useParams();
+
   const [cakes, setCakes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadData = async () => {
       const contract = await getContract();
 
+      const filtered = ProductCatalog.filter((cake) =>
+        category ? cake.slug.startsWith(category) : true
+      );
+
       const final = await Promise.all(
-        ProductCatalog.map(async (cake) => {
+        filtered.map(async (cake) => {
           try {
             const onChain = await contract.getCake(cake.id);
 
             return {
               ...cake,
-              price: ethers.formatEther(onChain.price), 
+              price: Number(
+                ethers.formatEther(onChain.price)
+              ).toFixed(3),
               isAvailable: onChain.isAvailable,
             };
-          } catch (err) {
+          } catch {
             return {
               ...cake,
               price: "0",
@@ -31,23 +41,30 @@ function Cakes() {
       );
 
       setCakes(final);
+      setLoading(false);
     };
 
     loadData();
-  }, []);
+  }, [category]);
+
+  if (loading) return <h3>Loading blockchain...</h3>;
 
   return (
     <div className="container mt-4">
       <div className="row">
         {cakes.map((cake) => (
-          <div key={cake.id} className="col-md-3">
-            <div className="card">
-              <img src={cake.img} alt="" />
+          <div key={cake.id} className="col-md-3 mb-3">
+            <div className="card h-100 text-center">
+              <img src={cake.img} alt={cake.name} />
+
               <h5>{cake.name}</h5>
+
               <p>{cake.price} ROSE</p>
 
               {!cake.isAvailable && (
-                <span style={{ color: "red" }}>Hết hàng</span>
+                <span style={{ color: "red" }}>
+                  Hết hàng
+                </span>
               )}
             </div>
           </div>
